fa9b5c3c4e397a95b009c8dbcaa45a3866ad5ce3
stbugyei <stbugyei@yahoo.co.uk>
2020-02-27 15:21:37 +0200

    css
    changed font size

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className="App"> 
       <header className="App-header">  
         <Conditionals /> 
       </header>
