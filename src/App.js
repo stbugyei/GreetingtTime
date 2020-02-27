18c5d727c4256c89a12aa38f9866db77c7c0b3ed
stbugyei <stbugyei@yahoo.co.uk>
2020-02-27 15:10:36 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Conditionals from "./components/Conditionals";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <header className="App-header">  
+        <Conditionals /> 
+      </header>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/DayBreak.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+  
+function Time() {
+    const months = ["JAN", "FEB", "MAR","APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+    let current_datetime = new Date()
+    let formatted_date = current_datetime.getDate() + "-" + months[current_datetime.getMonth()] + "-" + current_datetime.getFullYear() + ' ' + current_datetime.getHours() + ':' + current_datetime.getMinutes() + ':' + current_datetime.getSeconds();
+   // console.log(formatted_date)
+    return (
+        <div ><p>{formatted_date}</p>
+        </div>     
+    );
+  }  
+export default Time;
